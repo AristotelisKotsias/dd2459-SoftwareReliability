2206d1dbc1b4214e47d0027d1ce9017572c6cba4
AristotelisKotsias <kotsias.a@gmail.com>
2020-05-12 17:37:55 +0200

    changes

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -1,87 +1,67 @@
 import java.lang.IllegalArgumentException;
+import java.util.Arrays;
 
-/**
- * @author zacharias erlandsson
- * Class containing several different Array Operations (binary searching, sorting
- * searching an unsorted array, membership queries). The class is meant to be used in
- * a static context and not as instances.
- */
 public class ArrayOperations {
   public static void main(String[] args) {
     int[] a = new int[]{0, 132, 1024, 402, 1, 1};
     sort(a);
-    System.out.println(search(a, 0));
+    System.out.println(Arrays.toString(a));
+
+    System.out.println(binarySearch(a, 0, a.length - 1, 132));
   }
 
-  /**
-  * Method that sorts an integer-array of arbitrary length using the algorithm
-  * Selection Sort.
-  * @param A integer-array of arbitrary length
-  */
-  public static void sort(int[] A) {
-    if (A.length == 0) {
+  public static void sort(int arr[])
+  {
+    int n = arr.length;
+
+    if (n == 0) {
       //throw new IllegalArgumentException("Array must not be empty"); // Commented this out as mutation
     }
-    int currMin;
-    int currMinIdx;
-    int pivotValueOld;
-    for (int pivot = 0; pivot < A.length-1; pivot++){ // Changed to ".. pivot < A.length -2" in mutation
-      currMin = A[pivot];
-      currMinIdx = pivot;
-      pivotValueOld = A[pivot];
-      for (int i = pivot; i < A.length; i++){
-    	if (A[i] < currMin) { // Changed to >= in mutation
-          currMin = A[i];
-          currMinIdx = i;
-        }
-      }
-      A[pivot] = currMin;
-      A[currMinIdx] = pivotValueOld;
-    }
-    return;
-  }
 
-  /**
-  * Method that searches a sorted integer-array of arbitrary size for a value key
-  * using the binary search algorithm.
-  * @param A integer array of arbitrary length. Needs to be sorted.
-  * @param key integer value to be searched for in the sorted array A
-  * @return returns the index of the value key if it exists in the array, otherwise
-  * it returns -1.
-  */
-  public static int search(int[] A, int key) {
-    if (A.length == 0) {
-      throw new IllegalArgumentException("Array must not be empty");
-    }
-    int x; 
-    int l = 0; // changed to 1 in mutation 
-    int r = A.length - 1;
+    // One by one move boundary of unsorted subarray
+    for (int i = 0; i < n-1; i++)
+    {
+      // Find the minimum element in unsorted array
+      int min_idx = i;
+      for (int j = i+1; j < n; j++) // Changed to ".. j < n-1" in mutation
+        if (arr[j] < arr[min_idx]) // Changed to >= in mutation
+          min_idx = j;
 
-    do {
-      x = (l+r) / 2;
-      if (key < A[x]) {
-    	r = x - 1; //changed to r = x -2 in mutation 
-      } else {
-        l = x + 1; //changed to l = x + 2 in mutation
-      }
-    } while (key != A[x] && l <= r);
-
-    if (key == A[x]) {
-      return x;
-    } else {
-      return -1;
+      // Swap the found minimum element with the first
+      // element
+      int temp = arr[min_idx];
+      arr[min_idx] = arr[i];
+      arr[i] = temp;
     }
   }
 
-  /**
-  * Method that evaluates however an integer value key is present in an integer array A.
-  * Makes use of the search()-method.
-  * @param A integer array of arbitrary length. Needs to be sorted.
-  * @param key integer value to be evaluated for membership in the sorted array A.
-  * @return returns true if the value key is present in the array, and false if it isn't.
-  */
+  public static int binarySearch(int arr[], int l, int r, int x)
+  {
+    if (r >= l) {
+      int mid = l + (r - l) / 2;
+
+      // If the element is present at the
+      // middle itself
+      if (arr[mid] == x)
+        return mid;
+
+      // If element is smaller than mid, then
+      // it can only be present in left subarray
+      if (arr[mid] > x)
+        return binarySearch(arr, l, mid - 1, x);
+
+      // Else the element can only be present
+      // in right subarray
+      return binarySearch(arr, mid + 1, r, x);
+    }
+
+    // We reach here when element is not present
+    // in array
+    return -1;
+  }
+
   public static boolean membership(int[] A, int key) {
-    int index = search(A, key);
+    int index = binarySearch(A, 0, A.length-1, key);
     if (index >= 0) {
       return true;
     } else {
@@ -89,13 +69,6 @@ public class ArrayOperations {
     }
   }
 
-  /**
-  * Method that evaluates however an integer value key is present in an integer array A.
-  * Makes use of the search()-method and sort()-method.
-  * @param A integer array of arbitrary length. Doesn't need to be sorted.
-  * @param key integer value to be evaluated for membership in the array A.
-  * @return returns true if the value key is present in the array, and false if it isn't.
-  */
   public static boolean membershipUnsorted(int[] A, int key) {
     sort(A);
     return membership(A, key);
--- src/ArrayOperationsTestPairwise.java
+++ src/ArrayOperationsTestPairwise.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 //import com.google.code.tempusfugit.concurrency.IntermittentTestRunner;
 
